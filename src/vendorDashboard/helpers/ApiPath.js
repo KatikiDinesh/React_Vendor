c72dbe579eaecd7ca293402bf13b83053e843b1e
katikiDinesh <dineshkatiki9@gmail.com>
2025-11-12 10:36:33 +0530

    changed the path url to live path url

--- src/vendorDashboard/helpers/ApiPath.js
+++ src/vendorDashboard/helpers/ApiPath.js
@@ -1,3 +1,3 @@
-export const API_URL="http://localhost:4000"
+//export const API_URL="http://localhost:4000"
 
-//const API_URL="https://backend-mern-app-pven.onrender.com"
+const API_URL="https://backend-mern-app-pven.onrender.com"
